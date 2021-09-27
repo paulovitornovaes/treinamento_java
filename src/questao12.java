af0898f0617db1f290b9a8ae91443433b77f98a2
Paulo Vitor <72573443+paulovitornovaes@users.noreply.github.com>
2021-09-27 11:51:13 -0300

    commit

--- src/questao12.java
+++ src/questao12.java
@@ -1,7 +1,27 @@
 import java.util.Scanner;
 
 public class questao12 {
+    char tabuleiro[][] = new char[3][3];
+
     public static void main(String[] args) {
-        int[][] tabuleiro = new int[3][3];
+        Scanner teclado = new Scanner(System.in);
+        for (int contador = 0; contador < 9; contador++) {
+            if (contador == 0) {
+                System.out.println("Na primeira jogada para ganhar voce pode\n" +
+                        "Jogar na posição [0][0] - [0][2]" +
+                        "[2][0] - [2][2]");
+            }
+            System.out.println("\nJogador 1 escolha onde jogar: ");
+            char jogador1 = teclado.next().charAt(0);
+        }
+    }
+
+    public void imprimir_tabuleiro() {
+        for (int i = 0; i < 3; i++) {
+            System.out.print("\n");
+            for (int j = 0; j < 3; j++) {
+                System.out.println(tabuleiro[i][j]);
+            }
+        }
     }
 }
